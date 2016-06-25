ae3b43b7e67b95359402bb1b370ae1be0603c46c
Alistair Martin <alistairmartin01@gmail.com>
2016-06-25 21:45:02 +0930

    Include host/domain for the short_url in the json returned when a new url is added

--- routes/newUrl.js
+++ routes/newUrl.js
@@ -10,49 +10,41 @@ router.get('/', (req, res) => {
 });
 
 router.get(/.*/i, (req, res) => {
-  const originalUrl = req.url.slice(1);
+  const originalUrl = req.url.slice(1)
+      , { protocol, headers: { host }} = req
+      , domain = protocol + '://' + host;
 
   if ( URLRegExp.validate(originalUrl) ) {
     mongo.connect(url, (err, db) => {
       if (err) {
         console.error('There was a problem connecting to the db', err);
       }
-      getShortUrlAndInsert(db, res, originalUrl);
+      // fetch counter
+      db.collection('counter').findOneAndUpdate({}
+        , {$inc: {count:1}}
+        , {
+          returnOriginal: false,
+          upsert: true
+        }
+        , (err, counter) => {
+          if (err) console.error(err);
+          const shortUrl = counter.value.count;
+
+          // insert into db
+          db.collection('urls').insert({shortUrl, originalUrl}, (err, doc) => {
+            if (err) console.error(err);
+            res.json({
+              original_url: originalUrl,
+              short_url: domain + '/' + shortUrl,
+            });
+            db.close();
+          });
+        }
+      );
     });
   } else {
     res.json({error: '"' + originalUrl + '" is not a valid url'});
   }
 });
-
-
-function getShortUrlAndInsert(db, res, originalUrl) {
-  // fetch counter
-  db.collection('counter').findOneAndUpdate({}
-    , {$inc: {count:1}}
-    , {
-      returnOriginal: false,
-      upsert: true
-    }
-    , (err, doc) => {
-      if (err) console.error(err);
-      insertUrl(db, res, doc.value.count, originalUrl);
-    }
-  );
-}
-
-function insertUrl(db, res, shortUrl, originalUrl) {
-  // insert into db
-  db.collection('urls').insert({
-    shortUrl,
-    originalUrl
-  }, (err, doc) => {
-    if (err) console.error(err);
-    res.json({
-      original_url: originalUrl,
-      short_url: shortUrl,
-    });
-    db.close();
-  });
-}
 
 module.exports = router;
